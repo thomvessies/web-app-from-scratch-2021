c721607af2076c0c3bad2b94b0af04afd008a2be
thomvessies <t.vessies98@gmail.com>
2021-02-08 09:20:05 +0100

    API verbinding gemaakt

--- js/app.js
+++ js/app.js
@@ -1,41 +1,34 @@
-//source: https://github.com/taniarascia/sandbox/blob/master/ghibli/scripts.js
 
-const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
+const auth_link = "https://www.strava.com/oauth/token"
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+function getActivities(res){
 
-app.appendChild(logo);
-app.appendChild(container);
+  const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+  fetch(activities_link)
+    .then((res) => console.log(res.json()))
 
-var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
-
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
-
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
 }
+
+
+function reAuthorize(){
+  fetch(auth_link, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+
+    },
+
+    body: JSON.stringify({
+      client_id: '61340',
+      client_secret: 'f6133c63901944505b8c2b9f259a242df73c1f5b',
+      refresh_token: 'ec7b874a2cc9a3639955e1d312568c9df2f31129',
+      grant_type: 'refresh_token'
+
+    })
+  }).then(res => (res.json()))
+      .then(res => getActivities(res))
+}
+
+reAuthorize()
