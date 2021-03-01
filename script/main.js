0cec32cf0d0b1f843f8a02efdfcdcfadaf1c8b6a
thomvessies <t.vessies98@gmail.com>
2021-03-01 02:25:26 +0100

    Changed navbar and improved overall css

--- script/main.js
+++ script/main.js
@@ -3,28 +3,30 @@ import './vendor/router.js';
 import './modules/input.js';
 
 function useApiData(buttonNumber, data){
-    
     if (buttonNumber == 1){
         var data_explanation = document.querySelector("#Default_page")
         data_explanation.innerHTML += "<br>"
         data_explanation.innerHTML += data.explanation
+        data_explanation.innerHTML += "<br>"
         data_explanation.innerHTML += data.media_type == "video" ? `<iframe width="560" height="315" src="${data.url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` : `<img src="${data.url}">`
+        
     } else if (buttonNumber == 2){
         var data_explanation = document.querySelector("#PickSingleDay_page")
         data_explanation.innerHTML += "<br>"
         data_explanation.innerHTML += data.explanation
+        data_explanation.innerHTML += "<br>"
         data_explanation.innerHTML += data.media_type == "video" ? `<iframe width="560" height="315" src="${data.url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` : `<img src="${data.url}">`
     }
-    else if (buttonNumber == 4){
+    else if (buttonNumber == 3){
         var i
         for (i = 0; i < data.length; i++) {
             var data_explanation = document.querySelector("#StartDateEndDate_page")
-           data_explanation.innerHTML += "<br>"
-           data_explanation.innerHTML += data[i].explanation
-          data_explanation.innerHTML += data[i].media_type == "video" ? `<iframe width="560" height="315" src="${data[i].url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` : `<img src="${data[i].url}">`
+            data_explanation.innerHTML += "<br>"
+            data_explanation.innerHTML += data[i].explanation
+            data_explanation.innerHTML += "<br>"
+            data_explanation.innerHTML += data[i].media_type == "video" ? `<iframe width="560" height="315" src="${data[i].url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` : `<img src="${data[i].url}">`
         }
     }
-    
 }
 
 export { useApiData }
